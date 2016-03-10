0fca1b22698fe98583d401adc36e7f023b61830f
Florian Jug <jug@mpi-cbg.de>
2016-03-10 22:40:51 +0100

    Improved leave one out for ICPR dataset
    - subsampling
    - adding all pixel having GT class <= param
    - smaller modifications for convenience

--- regression/ICPR_Lymphocytes/icprLeaveOneOutRegressor.py
+++ regression/ICPR_Lymphocytes/icprLeaveOneOutRegressor.py
@@ -18,7 +18,7 @@ from tifffile import *
 import pylab
 from sklearn.externals import joblib
 
-def train( featureStackFilenames, groundTruthFilenames, doClassEqualization=False ):
+def train( featureStackFilenames, groundTruthFilenames, subsample_rate=1.0, addAllBelowEqual=-1, numTrees=200 ):
   assert len(featureStackFilenames)==len(groundTruthFilenames)
 
   for i,fnF in enumerate(featureStackFilenames):
@@ -40,27 +40,20 @@ def train( featureStackFilenames, groundTruthFilenames, doClassEqualization=Fals
     X_i = imF.reshape( (n_pixels, n_features) )
     y_i = imG.reshape( (n_pixels) )
 
-    # add 0 and 1 distances more then only one time
-    if doClassEqualization:
-        print y_i.size,
-        X_i_0 = X_i[y_i==0]
-        y_i_0 = y_i[y_i==0]
-        X_i_01 = X_i[y_i<=1]
-        y_i_01 = y_i[y_i<=1]
-        # raise Exception
-        X_i=np.concatenate( (X_i,np.concatenate( (X_i_01, X_i_0) ,axis=0)) ,axis=0)
-        y_i=np.concatenate( (y_i,np.concatenate( (y_i_01, y_i_0) ,axis=0)) ,axis=0)
+    print 'Max. samples:   ', y_i.size
 
-    print y_i.size,
+    # Subsample to save time and memory
+    elements_to_take = np.random.rand(y_i.size)<subsample_rate
+    X_i = X_i[elements_to_take]
+    y_i = y_i[elements_to_take]
 
-    # filter out useless training data
-    # max_dist = y_i.max()
-    # elements_to_take = y_i<max_dist
-    # elements_to_take = np.logical_or( elements_to_take, np.random.rand(y_i.size)<0.01 )
-    # X_i = X_i[elements_to_take]
-    # y_i = y_i[elements_to_take]
+    # add all samples below and euqal to a certain GT value
+    X_i_all = X_i[y_i<=addAllBelowEqual]
+    y_i_all = y_i[y_i<=addAllBelowEqual]
+    X_i=np.concatenate( (X_i,X_i_all) ,axis=0 )
+    y_i=np.concatenate( (y_i,y_i_all) ,axis=0 )
 
-    print y_i.size
+    print 'Samples to add: ', y_i.size
 
     if 'X' in locals():
       X = np.concatenate((X,X_i))
@@ -69,13 +62,13 @@ def train( featureStackFilenames, groundTruthFilenames, doClassEqualization=Fals
       X = X_i
       y = y_i
 
-  # params = {'n_estimators': 500, 'max_depth': 4, 'min_samples_split': 1,
+  # params = {'n_estimators': numTrees, 'max_depth': 4, 'min_samples_split': 1,
             # 'learning_rate': 0.01, 'loss': 'lad', 'verbose': 3}
   # clf = ensemble.GradientBoostingRegressor(**params)
   # clf.fit(X, y)
   # return clf, X, y
 
-  params = {'n_estimators': 50, 'max_depth': None, 'min_samples_split': 2,
+  params = {'n_estimators': numTrees, 'max_depth': None, 'min_samples_split': 2,
             'n_jobs': 8, 'verbose': 3}
   rfr = ensemble.RandomForestRegressor(**params)
   rfr.fit(X, y)
@@ -119,9 +112,10 @@ def log_done():
 # MAIN MAIN MAIN MAIN MAIN MAIN MAIN MAIN MAIN MAIN MAIN MAIN MAIN MAIN MAIN 
 # ==========================================================================
 log('START')
-PLOT = True
+PLOT = False
 
-filenamePrefixModel = 'rfRegressorOnAll_noSparse_100trees_leave'
+numTrees = 200
+filenamePrefixModel = 'rfRegressorOnAll_noSparse_'+str(numTrees).zfill(2)+'trees_leave'
 
 folderModels = '/Users/jug/ownCloud/ProjectRegSeg/data/Histological/ICPR_Lymphocytes/models/leaveOneOuts/'
 folderPredict = '/Users/jug/ownCloud/ProjectRegSeg/data/Histological/ICPR_Lymphocytes/predictions/'
@@ -134,16 +128,15 @@ folderGtTrain = '/Users/jug/ownCloud/ProjectRegSeg/data/Histological/ICPR_Lympho
 trainFeatureFiles = [ os.path.join(folderFeaturesTrain, fn) for fn in os.listdir(folderFeaturesTrain) if fn.endswith('.tif') ]
 trainGtFiles = [ os.path.join(folderGtTrain, fn) for fn in os.listdir(folderGtTrain) if fn.endswith('.tif') ]
 
-# print trainFeatureFiles
-# print trainGtFiles
-
 # -- TRAIN -- TRAIN -- TRAIN -- TRAIN -- TRAIN -- TRAIN -- TRAIN -- TRAIN -- 
 for i in range(len(trainFeatureFiles)): # leave one out loop
     log_start( 'Start training, leaving out '+str(i+1)+' of '+str(len(trainFeatureFiles))+'... ' )
-    regressor = train(
+    regressor, X, y = train(
             trainFeatureFiles[:i]+trainFeatureFiles[(i+1):],
             trainGtFiles[:i]+trainGtFiles[(i+1):],
-            doClassEqualization=True)
+            subsample_rate=0.25,
+            addAllBelowEqual=2,
+            numTrees=numTrees )
     log_done()
     log_start( 'Start writing model to "'+folderModels+filenamePrefixModel+str(i)+'"... ' )
     joblib.dump(regressor, folderModels+filenamePrefixModel+str(i)+'.pkl')
@@ -154,7 +147,7 @@ for i in range(len(trainFeatureFiles)): # leave one out loop
     # ---------------------------------------------------------------------------------------
     testFeatureFiles = [ trainFeatureFiles[i] ]
     log_start( 'Start regressing... ' )
-    predImgs = regress( regressor[0], testFeatureFiles )
+    predImgs = regress( regressor, testFeatureFiles )
     log_done()
 
     # -- WRITE RESULTS -- WRITE RESULTS -- WRITE RESULTS -- WRITE RESULTS -- 
